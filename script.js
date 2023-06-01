d1e0fa136be007586ee8b142430f98b7e910317b
sanjeetsk <ssanjeetkumar67@gmail.com>
2023-06-01 11:15:47 +0530

    commit

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ function convertToRoman(num) {
   //your code here
   let roman = '';
    
-  for (let i = 0; i < obj.length; i++) {
+  for (let i in obj) {
     while (num >= obj[i][1]) {
       roman += obj[i][0];
       num -= obj[i][1];
@@ -30,10 +30,10 @@ function convertToRoman(num) {
 }
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
-console.log(convertToRoman(36));
+console.log(convertToRoman(900));
 
 
 
 
 // do not edit below this line
-module.exports = convertToRoman
+// module.exports = convertToRoman
